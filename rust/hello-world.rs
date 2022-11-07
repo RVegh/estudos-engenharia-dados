8e13115e8bb78d4da736b9a851ead6b49b78d6d6
Ricardo vegh <ricardovegh@hotmail.com>
2022-11-07 16:12:03 -0300

    initial commit

---
+++ rust/hello-world.rs
@@ -0,0 +1,8 @@
+// How to comment in Rust:
+    /* Block comments which go to the closing delimiter. */
+    // Line comments which go to the end of the line.
+
+fn main() {
+    println!("My first hello World in Rust: Hello World!");
+}
+
